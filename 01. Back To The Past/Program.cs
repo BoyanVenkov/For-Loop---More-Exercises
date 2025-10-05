f676737aba7ff61d1c32e8c2d9680f54ea30d296
Boyan Venkov <boyanvenkov@yahoo.com>
2025-10-05 10:51:46 +0300

    Add project files.

---
+++ 01. Back To The Past/Program.cs
@@ -0,0 +1,29 @@
+﻿double inheritance = double.Parse(Console.ReadLine());
+double year = double.Parse(Console.ReadLine());
+
+double oddMoney = 0;
+double totalMoney = 0;
+double age = 18;
+
+for (int i = 1800; i<=year; i++)
+{
+    if (i % 2 == 0)
+    {
+        totalMoney += 12000;
+        age++;
+    }
+    else
+    {
+        oddMoney = 12000 + 50 * age;
+        totalMoney += oddMoney;
+        age++;
+    }
+}
+if (inheritance >= totalMoney)
+{
+    Console.WriteLine($"Yes! He will live a carefree life and will have {inheritance - totalMoney:f2} dollars left.");
+}
+else 
+{
+    Console.WriteLine($"He will need {totalMoney - inheritance:f2} dollars to survive.");
+}
---
+++ 02. Hospital/Program.cs
@@ -0,0 +1,30 @@
+﻿int days = int.Parse(Console.ReadLine());
+int doctors = 7;
+int daysInHospital = 1;
+int patientsExamined = 0;
+int patientsUnExamined = 0;
+
+
+
+for (int i = 1; i<= days; i++)
+{
+    int patients = int.Parse(Console.ReadLine());
+
+    if (i % 3 == 0 && doctors <= patientsUnExamined)
+    {
+        doctors++;
+    }
+    if (doctors >= patients)
+    {
+        patientsExamined += patients;
+    }
+    if (patients > doctors)
+    {
+        patientsUnExamined += patients - doctors;
+        patientsExamined += doctors;
+    }
+    daysInHospital++;
+
+}
+Console.WriteLine($"Treated patients: {patientsExamined}.");
+Console.WriteLine($"Untreated patients: {patientsUnExamined}.");
