4f301a771b75b7a862ed6a0c098ebf2001d07659
Boyan Venkov <boyanvenkov@yahoo.com>
2025-10-07 16:24:03 +0300

    Added a New Project

---
+++ 08. Equal Pairs/Program.cs
@@ -0,0 +1,37 @@
+﻿int pairs = int.Parse(Console.ReadLine());
+
+int previousSum = 0;
+int equalSum = 0;
+int maxDiff = 0;
+int diff = 0;
+for (int i = 1; i <= pairs; i++)
+{
+    int firstNum = int.Parse(Console.ReadLine());
+    int secondNum = int.Parse(Console.ReadLine());
+    int currentSum = secondNum + firstNum;
+    if (i == 1)
+    {
+        equalSum = currentSum;
+        previousSum = currentSum;
+    }
+    else
+    {
+        diff = Math.Abs(currentSum - previousSum);
+    }
+       
+    if (maxDiff < diff)
+    {
+        maxDiff = diff;
+    }
+
+  
+    previousSum = currentSum;
+}
+if (maxDiff == 0)
+{
+    Console.WriteLine($"Yes, value={equalSum}");
+}
+else
+{
+    Console.WriteLine($"No, maxdiff={maxDiff}");
+}
